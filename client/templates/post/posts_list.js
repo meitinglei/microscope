1966a409e02900a201672862c698afc25bf14501
lewis <meitinglei@163.com>
2015-06-16 12:34:43 +0800

    简单的路由

--- client/templates/post/posts_list.js
+++ client/templates/post/posts_list.js
@@ -1,21 +1,5 @@
-var postsData = [
-    {
-        title: 'Introducing Telescope',
-        url: 'http://sachagreif.com/introducing-telescope/'
-    },
-    {
-        title: 'Meteor',
-        url: 'http://meteor.com'
-    },
-    {
-        title: 'The Meteor Book',
-        url: 'http://themeteorbook.com'
-    },
-    {
-        title: 'dota2shop',
-        url: 'http://www.dota2shop.cn'
-    }
-];
 Template.postsList.helpers({
-    posts: postsData
+    posts: function() {
+        return Posts.find();
+    }
 });
